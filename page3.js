cbe87bdea1f92bf826ec4da9ef7db31702394655
Dyan1212 <tnguy384@asu.edu>
2024-11-27 01:37:42 -0700

    page 3 run (did not finish)

--- page3.js
+++ page3.js
@@ -1,20 +1,33 @@
 let rects = [];
+let oders = [1,2,3,4,5,6,1,2,3,4,5,6];
 
 function setupPage3(){
     borderW = 60;
     borderH = 90;
     wRec = w - 2 * borderW;
     hRec = h - 2 * borderH;
-    shapeWidth = wRec / 5 - 40;
-    shapeHeight = hRec / 2 -40;
+    boxHeight = hRec / 2 - 40;
+    boxWidth = wRec / 6 - 40;
+    shape = boxWidth * 2 / 3;
 
-    let disW = wRec / 5;
+    let disW = wRec / 6;
     let disH = hRec / 2;
 
-    for (let i = 0; i < 5; i++){
-        rects[i] = new Rect(borderW + disW / 2 + i * disW, borderH + disH / 2, shapeWidth, shapeHeight);
-        rects[i+5] = new Rect(borderW + disW / 2 + i * disW, borderH + disH / 2 + disH, shapeWidth, shapeHeight);
+    oders = shuffle(oders);
+
+    console.log(oders);
+
+    for (let i = 0; i < 2; i++){
+        rects[i] = [];
+        for (let j = 0; j < 6; j++){
+            if (i == 0){
+                rects[i][j] = new Rect(borderW + disW / 2 + j * disW, borderH + disH / 2 + disH * i, boxWidth, boxHeight,shape,oders[j]);
+            } else {
+                rects[i][j] = new Rect(borderW + disW / 2 + j * disW, borderH + disH / 2 + disH * i, boxWidth, boxHeight,shape,oders[j + 6]);
+            }            
+        }
     }
+    
 
 }
 
@@ -35,42 +48,181 @@ function drawPage3(){
     textAlign(CENTER, TOP);
     text("Match the shapes", w/2, 30);
     
-    for (let i = 5; i < 10; i++)
-        rects[i].draw();
-      
-      for (let i = 0; i < 5; i++)
-        rects[i].draw();
+    for (let i = 0; i < 2; i++){
+        for (let j = 0; j < 6; j++){
+            rects[i][j].draw();
+        }
+    }
 
+ 
 }
 
 class Rect {
-    constructor(x,y,wid,hei){
+    constructor(x,y,wid,hei,shape,type){
         this.x = x;
         this.y = y;
         this.hei = hei;
         this.wid = wid;
+        this.shape = shape;
+        this.type = type;
     }
 
     drawRectangleAtCenter(x,y,wid,hei){
-        
+        rect(x - wid/2,y - hei/2,wid,hei); 
+    }
+
+    drawBox(x,y,wid,hei){
         fill(228,243,243);
+        strokeWeight(2);
         stroke(228,243,243);
-        rect(x - wid/2,y - hei/2,wid,hei);
+        this.drawRectangleAtCenter(x,y,wid,hei);
         fill(228,243,243);
         stroke(146, 216, 212);
-        rect(x - wid/2 + 6,y - hei/2 + 6,wid - 12,hei - 12);
+        this.drawRectangleAtCenter(x,y,wid - 12,hei - 12);
 
         fill(146, 216, 212);
         textFont('Verdana');
         noStroke();
         textSize(wid - 20);
         text("?", x , y - (wid - 20) / 2 );
+    }
+
+    drawFlippedBox(x,y,wid,hei){
+        fill(228,243,243);
+        stroke(228,243,243);
+        this.drawRectangleAtCenter(x,y,wid,hei);        
+    }
+
+    drawTriangleCenterAt(x, y, len){
+        let v1 = createVector(0, -len / Math.sqrt(3)); 
+        let v2 = v1.copy();
+        v2.rotate(PI / 3 * 2);
+        let v3 = v1.copy();
+        v3.rotate(-PI / 3 * 2);
+        triangle(x + v1.x, y + v1.y, x + v2.x, y + v2.y, x + v3.x, y + v3.y);
+    }
+      
+    drawStarCenterAt(x, y, len){
+        len /= 1.5;
+        let v1 = createVector(0, -len);
+        let v2 = createVector(0, len / 3);
+        let a = 2 * PI / 5;
+        v2.rotate(a * 3);
         
+        beginShape();
+        noStroke();
+        for (let i = 0; i < 5; i++){
+        
+            vertex(x + v1.x, y + v1.y);
+            vertex(x + v2.x, y + v2.y);
+            
+            v1.rotate(a);
+            v2.rotate(a);
+        }
+        endShape(CLOSE);
+
+    }
+
+    drawXInBox(x,y,wid,hei){
+
+        fill(228,243,243);
+        this.drawFlippedBox(x,y,wid,hei);
+        fill(146, 216, 212);
+        textFont('PL Benguiat Frisky');
+        noStroke();
+        textSize(wid - 20);
+        text("x", x , y - (wid - 20) / 2 );
+
+    }
+
+    drawPlusInBox(x,y,wid,hei){
+
+        fill(228,243,243);
+        this.drawFlippedBox(x,y,wid,hei);
+        fill(146, 216, 212);
+        textFont('PL Benguiat Frisky');
+        noStroke();
+        textSize(wid - 20);
+        text("+", x , y - (wid - 20) / 2 );
+        
+    }
+
+    drawSquareInBox(x,y,wid,hei,shape){
+
+        fill(228,243,243);
+        this.drawFlippedBox(x,y,wid,hei);
+        fill(228,243,243);
+        strokeWeight(5);
+        stroke(146, 216, 212);
+        this.drawRectangleAtCenter(x,y,shape,shape);
+        noStroke();
+        // fill(228,243,243);
+        // this.drawRectangleAtCenter(x,y, shape * 11 / 12, shape * 11 / 12);
+
+    }
+
+
+    drawTriangleInBox(x,y,wid,hei,shape){
+
+        fill(228,243,243);
+        this.drawFlippedBox(x,y,wid,hei);
+        fill(228,243,243);
+        strokeWeight(5);
+        stroke(146, 216, 212);
+        this.drawTriangleCenterAt(x,y,shape);
+        noStroke();
+
+    }
+
+    drawStarInBox(x,y,wid,hei,shape){
+
+        fill(228,243,243);
+        this.drawFlippedBox(x,y,wid,hei);
+        fill(146, 216, 212);
+        this.drawStarCenterAt(x,y,shape);
+        fill(228,243,243);
+        this.drawStarCenterAt(x,y, shape * 9 / 12, shape * 9 / 12);
+
+    }
+
+    drawCircleInBox(x,y,wid,hei,shape){
+
+        fill(228,243,243);
+        this.drawFlippedBox(x,y,wid,hei);
+        fill(228,243,243);
+        strokeWeight(5);
+        stroke(146, 216, 212);
+        circle(x,y,shape);
+        noStroke();
     }
 
     draw(){
 
-        this.drawRectangleAtCenter(this.x, this.y, this.wid, this.hei);
+        this.drawBox(this.x, this.y, this.wid, this.hei);
+        if(dist(this.x,this.y,mouseX,mouseY) < this.wid){
+            switch(this.type){
+                case 1:
+                    this.drawXInBox(this.x, this.y, this.wid, this.hei);
+                    break;
+                case 2:
+                    this.drawPlusInBox(this.x, this.y, this.wid, this.hei);
+                    break;
+                case 3:
+                    this.drawSquareInBox(this.x, this.y, this.wid, this.hei, this.shape);
+                    break;
+                case 4:
+                    this.drawTriangleInBox(this.x, this.y, this.wid, this.hei, this.shape);
+                    break;
+                case 5:
+                    this.drawStarInBox(this.x, this.y, this.wid, this.hei, this.shape);
+                    break;
+                case 6:
+                    this.drawCircleInBox(this.x, this.y, this.wid, this.hei, this.shape);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
 
